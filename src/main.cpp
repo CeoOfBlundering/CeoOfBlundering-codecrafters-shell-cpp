87651c911c614da9222981858aa24a8619872103
Pars Ege Uslu <parespars@gmail.com>
2026-07-09 22:24:36 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <filesystem>
+#include <cstdlib>
 
 #ifdef _WIN32
     const char PATH_DELIMITER = ';';
@@ -22,6 +23,7 @@ int main() {
     std::string path = path_env ? path_env : "";
 
     while (true) {
+        // Get Input
         std::cout << "$ ";
         if (!std::getline(std::cin, input)) {
             break;
@@ -36,8 +38,8 @@ int main() {
             continue;
         }
 
+        // Parse input
         size_t firstSpace = input.find(' ');
-
         if (firstSpace == std::string::npos) {
             command = input;
             argument = "";
@@ -47,6 +49,7 @@ int main() {
             argument = input.substr(firstSpace + 1);
         }
 
+        // Process Command
         if (command == "exit") {
             break;
         }
@@ -91,11 +94,45 @@ int main() {
                 }
             }
             else {
+
                 std::cout << argument << ": not found\n";
             }
         }
         else {
-            std::cout << command << ": command not found\n";
+            if (!path.empty()) {
+                std::vector<std::string> paths;
+                size_t start = 0, end = 0;
+                while (start <= path.length()) {
+                    end = path.find(PATH_DELIMITER, start);
+                    if (end == std::string::npos)
+                        end = path.length();
+                    paths.push_back(path.substr(start, end - start));
+                    start = end + 1;
+                }
+                bool found = false;
+                for (const std::string& p : paths) {
+                    fs::path full_path = fs::path(p) / command;
+
+                    if (!fs::exists(full_path) || !fs::is_regular_file(full_path)) {
+                        continue;
+                    }
+
+                    fs::perms permissions = fs::status(full_path).permissions();
+                    fs::perms executable = fs::perms::owner_exec | fs::perms::group_exec | fs::perms::others_exec;
+
+                    if ((permissions & executable) != fs::perms::none) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    std::system(input.c_str());
+                }
+                else
+                    std::cout << command << ": command not found\n";
+            }
+            else
+                std::cout << command << ": command not found\n";
         }
     }
     return 0;
